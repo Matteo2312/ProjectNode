4f668c48a499b5210fab2c677932a08a0ee92c49
jeatic <122202333+jeatic@users.noreply.github.com>
2023-12-29 10:38:35 +0100

    Update request.js
    Add all the SQL requests needed

--- request.js
+++ request.js
@@ -41,10 +41,44 @@ function executeQuery(sqlQuery, callback) {
         });
     });
 }
+//get all :
+const sqlQuery_all  = "SELECT * FROM projet_final.new_table ;";
 
-// Exemple d'utilisation de la fonction avec une requête SQL
-const sqlQueryExample = 'SELECT annee FROM projet_final.new_table;';
-executeQuery(sqlQueryExample, (error, results) => {
+
+//get by subject :
+const sqlQuery_math = "SELECT * FROM projet_final.new_table WHERE matiere LIKE '%math%';";
+const sqlQuery_philosophie = "SELECT * FROM projet_final.new_table WHERE matiere LIKE '%philosophie%';";
+const sqlQuery_physique = "SELECT * FROM projet_final.new_table WHERE matiere LIKE '%physique%';";
+const sqlQuery_francais = "SELECT * FROM projet_final.new_table WHERE matiere LIKE '%francais%';";
+const sqlQuery_histoire = "SELECT * FROM projet_final.new_table WHERE matiere LIKE '%histoire%';";
+
+//By year :
+const sqlQuery_2022 = `SELECT * FROM projet_final.new_table WHERE annee = 2022;`;
+const sqlQuery_2020 = `SELECT * FROM projet_final.new_table WHERE annee = 2020;`;
+const sqlQuery_2021 = `SELECT * FROM projet_final.new_table WHERE annee = 2021;`;
+const sqlQuery_2023 = `SELECT * FROM projet_final.new_table WHERE annee = 2023;`;
+
+//By dificulty :
+minDifficulte = 2;
+maxDifficulte = 4; //to be changed
+const sqlQuery_difficulty = `SELECT * FROM projet_final.new_table WHERE difficulte BETWEEN ${minDifficulte} AND ${maxDifficulte};`;
+
+
+//By ID
+sujetId = 6; //To be changed
+const sqlQuery_id = `SELECT * FROM projet_final.new_table WHERE id = ${sujetId};`;
+
+//By exa center :
+const sqlQuery_France = "SELECT * FROM projet_final.new_table WHERE centreExamen LIKE '%France%';";
+const sqlQuery_Polynesie = "SELECT * FROM projet_final.new_table WHERE centreExamen LIKE '%Polynesie%';";
+const sqlQuery_Asie = "SELECT * FROM projet_final.new_table WHERE centreExamen LIKE '%Asie%';";$
+const sqlQuery_Antille = "SELECT * FROM projet_final.new_table WHERE centreExamen LIKE '%Antille%';";
+
+
+
+sqlQuery = sqlQuery_all //Change of the query : test
+
+executeQuery(sqlQuery, (error, results) => {
     if (error) {
         console.error('Erreur : ' + error);
         return;
@@ -53,3 +87,4 @@ executeQuery(sqlQueryExample, (error, results) => {
     // Utilisez les résultats comme nécessaire
     console.log('Résultats de la requête :', results);
 });
+
